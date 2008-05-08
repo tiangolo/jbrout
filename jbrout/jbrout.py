8ea9ad25d5c9da88fa0c9b75f6fcc791a34bafca
manatlan <manatlan@b4d1b9c4-0e25-0410-a282-091a75307ab2>
2008-05-08 13:22:19 +0000

    - the return of search functions in a tab pane (like jbrout0.1) ... yes !
    git-svn-id: https://jbrout.googlecode.com/svn/trunk@105 b4d1b9c4-0e25-0410-a282-091a75307ab2

--- jbrout/jbrout.py
+++ jbrout/jbrout.py
@@ -18,6 +18,7 @@
 #TODO : code to load/save bookmarks ;-)
 
 import os
+import string
 import sys,time # to hack thread on win32
 
 try:
@@ -68,7 +69,6 @@ except:
 from jbrout.common import cd2rd,cd2d,format_file_size_for_display,runWith,openURL # for selecteur
 from jbrout.commongtk import AlbumCommenter,InputBox,MessageBox,InputQuestion,Img,WinKeyTag,colorToString
 from jbrout.db import JBrout,Buffer
-from jbrout.winsearch import Winsearch
 from jbrout.winshow import WinShow
 from jbrout.listview import ThumbnailsView
 from jbrout.externaltools import ExternalTools
@@ -1131,6 +1131,78 @@ class Window(GladeApp):
         # and set it
         self.mode = Window.MODEFOLDER
 
+
+
+
+        #$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$
+        # tab search init
+        #$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$
+        try:
+            min,max=JBrout.db.getMinMaxDates()
+        except:
+            min = datetime.now()
+        self.__begin= min
+        self.__end = datetime.datetime.now()
+
+        t=(self.__end - self.__begin).days +1
+        self.hs_from.set_range(0,t)
+        self.hs_to.set_range(0,t)
+
+        self.hs_from.set_increments(0.5, 30)
+        self.hs_to.set_increments(0.5, 30)
+        self.hs_from.set_value(1)
+        self.hs_from.set_value(0)
+        self.hs_to.set_value(t)
+
+
+        cell = gtk.CellRendererText()
+        self.cb_format.pack_start(cell, True)
+        self.cb_format.add_attribute(cell, 'text',0)
+
+        self.setSearchCombo(self.cb_format,["",_("Landscape"),_("Portrait")],0)
+
+        ###################
+        def filename(column, cell, model, iter):
+            cell.set_property('text', model.get_value(iter, 0))
+            cell.set_property('foreground', model.get_value(iter, 2))
+            cell.set_property('xalign', 0)
+            #~ cell.set_property('xpad', 1)
+        def pixbuf(column, cell, model, iter):
+            if model.get_value(iter, 3)==0:
+                cell.set_property('pixbuf', Buffer.pbCheckEmpty)
+            elif model.get_value(iter, 3)==1:
+                cell.set_property('pixbuf', Buffer.pbCheckInclude)
+            elif model.get_value(iter, 3)==2:
+                cell.set_property('pixbuf', Buffer.pbCheckExclude)
+            else:
+                cell.set_property('pixbuf', Buffer.pbCheckDisabled)
+            cell.set_property('width', 16)
+            cell.set_property('xalign', 0)
+
+        cellpb = gtk.CellRendererPixbuf()
+        cell = gtk.CellRendererText()
+        column = gtk.TreeViewColumn()
+        column.pack_start(cellpb, False)
+        column.pack_start(cell, True)
+        column.set_cell_data_func(cellpb, pixbuf)
+        column.set_cell_data_func(cell, filename)
+        ###################
+
+        self.tvSearch.append_column(column)
+        treeselection = self.tvSearch.get_selection()
+        treeselection.set_mode(gtk.SELECTION_NONE)
+
+
+        storeTags=TreeTags()
+        try:
+            self.tvSearch.set_model( storeTags )
+            storeTags.expander(self.tv_tags)
+            storeTags.cleanSelections()
+        except:
+            pass
+        #$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$
+        
+        
         # init some attributs
         self.storeMultipleSelectedPathsOfTags=[]
 
@@ -2042,14 +2114,14 @@ class Window(GladeApp):
             ExternalTools.generate(JBrout.toolsFile)
         runWith(["notepad.exe","leafpad","scite","gedit","kate"],unicode(JBrout.toolsFile))
 
-    def on_search_activate(self, widget, *args):
-        win_search = Winsearch( TreeTags(), self.main_widget )
-        win_search.loop()
-
-        if win_search.xpath:
-            libl,xpath = win_search.xpath
-            ln = JBrout.db.select(xpath)
-            self.SetSelection(libl,xpath,ln,Window.MODETAG)
+    #def on_search_activate(self, widget, *args):
+    #    win_search = Winsearch( TreeTags(), self.main_widget )
+    #    win_search.loop()
+    #
+    #    if win_search.xpath:
+    #        libl,xpath = win_search.xpath
+    #        ln = JBrout.db.select(xpath)
+    #        self.SetSelection(libl,xpath,ln,Window.MODETAG)
 
 
 
@@ -2584,6 +2656,179 @@ class Window(GladeApp):
                         #~ return ret
 
 
+
+
+
+
+
+    def on_tvSearch_button_press_event(self,widget,*args):
+        event=args[0]
+        tup= widget.get_path_at_pos( int(event.x), int(event.y) )
+        if tup:
+            path,obj,x,y = tup
+
+            if path:
+                model = widget.get_model()
+                iterTo = model.get_iter(path)
+                node = model.get(iterTo)
+
+                # let's find the x beginning of the cell
+                xcell = widget.get_cell_area(path, widget.get_column(0) ).x
+
+                if node.__class__.__name__ != "TagNode":
+                    # we are on a category, there is an arrow at the beginning of
+                    # the cell to set expand or collapse
+                    # we must shift the xcell beginning
+                    xcell+=16
+
+                if x>xcell:
+                    # click on the cell (not on the arrow)
+                    if event.button==1:
+                        model.switch_inc(iterTo)
+                    elif event.button==3:
+                        model.switch_exc(iterTo)
+                    return 1 # stop the propagation of the event
+                else:
+                    # click nowhere or on the arrow ;-)
+                    return 0 # let the event propagation
+
+
+        #~ event=args[0]
+        #~ click_info = widget.get_dest_row_at_pos( int(event.x), int(event.y) )
+        #~ if click_info:
+            #~ print event.type
+            #~ model = widget.get_model()
+            #~ path, position = click_info
+            #~ print position, type(position)
+
+            #~ iterTo = model.get_iter(path)
+            #~ if event.button==2:
+                #~ model.switch_inc(iterTo)
+            #~ elif event.button==3:
+                #~ model.switch_exc(iterTo)
+        
+    def on_tvSearch_row_activated(self,widget,*args):
+        treeselection = widget.get_selection()
+        model, iter0 = treeselection.get_selected()
+        if iter0:
+            model.switch(iter0)
+        
+    def on_hs_to_value_changed(self, widget, *args):
+        val=widget.get_value()
+        if val<self.hs_from.get_value():
+            self.hs_from.set_value(val)
+        d=self.getDateFromScale(val)
+        self.l_to.set_label( d.strftime( unicode(_("To %m/%d/%Y %A")).encode("utf_8") ) )
+
+    def on_hs_from_value_changed(self, widget, *args):
+        val=widget.get_value()
+        if val>self.hs_to.get_value():
+            self.hs_to.set_value(val)
+        d=self.getDateFromScale(val)
+        self.l_from.set_label(  d.strftime( unicode(_("From %m/%d/%Y %A")).encode("utf_8") ) )
+
+    def setSearchCombo(self,obj,list,n):
+        m=gtk.ListStore( str)
+        m.clear()
+        for i in list:
+            m.append( [i,] )
+        obj.set_model(m)
+        obj.set_active(n)
+
+    def getDateFromScale(self,val):
+        return (self.__begin + datetime.timedelta(days=val)).date()
+
+    def on_btn_search_clicked(self,widget,*args):
+        
+        def mkpcom(valRech):    # photo comment
+            valRech=unicode(valRech).encode("utf_16")
+            sfrom = u"ABCDEFGHIJKLMNOPQRSTUVWXYZàâäéèêëïîôöûùüç"
+            sto   = u"abcdefghijklmnopqrstuvwxyzaaaeeeeiioouuuc"
+            valRech = valRech.translate( string.maketrans(sfrom.encode("utf_16"),sto.encode("utf_16")) ).decode("utf_16")
+            return u"""contains(translate(c, "%s", "%s") ,"%s")""" % (sfrom,sto,valRech)
+        
+        def mkacom(valRech):    # album comment
+            valRech=unicode(valRech).encode("utf_16")
+            sfrom = u"ABCDEFGHIJKLMNOPQRSTUVWXYZàâäéèêëïîôöûùüç"
+            sto   = u"abcdefghijklmnopqrstuvwxyzaaaeeeeiioouuuc"
+            valRech = valRech.translate( string.maketrans(sfrom.encode("utf_16"),sto.encode("utf_16")) ).decode("utf_16")
+            return  u""" ( contains(translate(../c, "%s", "%s") ,"%s")""" % (sfrom,sto,valRech) +\
+                    u""" or contains(translate(../@name, "%s", "%s") ,"%s") )""" % (sfrom,sto,valRech)
+        
+        
+        
+        def string2ops(chaine,callback):
+            tmots=[]
+            mots=[]
+            l = chaine.strip().split(" ")
+            for i in range(len(l)):
+                mot = l[i].strip().lower()
+                if mot:
+                    if mot[0] == "-":
+                        mots.append( u"not(%s)" % callback(mot[1:]) )
+                        tmots.append( _(u"NOT %s") % mot[1:])
+                    else:
+                        mots.append( callback(mot) )
+                        tmots.append( mot )
+            return tmots,mots
+                
+        dt_from = self.getDateFromScale(self.hs_from.get_value())
+        dt_to = self.getDateFromScale(self.hs_to.get_value())
+
+        tops=[] #textual operands
+        ops=[]
+        if self.__begin.date()!=dt_from:
+            ops.append(u"substring(@date,1,8)>='%s'" % dt_from.strftime('%Y%m%d'))
+            tops.append(dt_from.strftime('From:%d/%m/%Y'))
+        if self.__end.date()!=dt_to:
+            ops.append(u"substring(@date,1,8)<='%s'" % dt_to.strftime('%Y%m%d'))
+            tops.append(dt_to.strftime('To:%d/%m/%Y'))
+
+        if self.cb_format.get_active()==1: # landscape
+            ops.append( u"substring-before(@resolution, 'x')>substring-after(@resolution, 'x')" )
+            tops.append(_("Format:Landscape"))
+        if self.cb_format.get_active()==2: # portrait
+            ops.append( u"substring-before(@resolution, 'x')<substring-after(@resolution, 'x')" )
+            tops.append(_("Format:Portrait"))
+
+        tcom,op = string2ops(self.e_pcom.get_text(), mkpcom)
+        if op:
+            ops+=op
+            tops.append(_("comment(%s)") % _(" and ").join(tcom) )
+        tcom,op=string2ops(self.e_acom.get_text(), mkacom)
+        if op:
+            ops+=op
+            tops.append(_("album(%s)") % _(" and ").join(tcom) )
+
+        store=self.tvSearch.get_model(  )
+        if store:
+            ll=store.getSelected()
+            for tcheck,nom,l in ll:
+                if type(l)==list:
+                    orList = u" or ".join([u"t='%s'"%i for i in l])
+                    if tcheck==1: #include
+                        ops.append( u"(%s)" % orList )
+                        tops.append(u"'%s'"%nom)
+                    else: # ==2 #exclude
+                        ops.append( u"not(%s)" % orList )
+                        tops.append(_(u"NOT '%s'")%nom)
+                else:
+                    op = u"t='%s'"%l
+                    if tcheck==1: #include
+                        ops.append( op )
+                        tops.append(u"'%s'"%nom)
+                    else: # ==2 #exclude
+                        ops.append( u"not(%s)" % op )
+                        tops.append(_(u"NOT '%s'")%nom)
+
+        if ops:
+            libl,xpath = u" and ".join(tops),u"//photo[%s]" % u" and ".join(ops)
+        else:
+            libl,xpath = _(u"ALL"),u"//photo"
+        
+        ln = JBrout.db.select(xpath)
+        self.SetSelection(libl,xpath,ln,self.mode)
+        
 def main(canModify=True):
     if JBrout.lockOn():
         try:
