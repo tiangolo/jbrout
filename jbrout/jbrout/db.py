d905ab4211703264b329750d42c4799d3af41915
manatlan <manatlan@b4d1b9c4-0e25-0410-a282-091a75307ab2>
2009-01-11 15:03:45 +0000

    - fix preceding patch, to correct unittests, and update the createNewFolder method. Now works better
    git-svn-id: https://jbrout.googlecode.com/svn/trunk@158 b4d1b9c4-0e25-0410-a282-091a75307ab2

--- jbrout/jbrout/db.py
+++ jbrout/jbrout/db.py
@@ -420,21 +420,25 @@ class FolderNode(object):
     def createNewFolder(self,newname):
         assert type(newname)==unicode
         newname = os.path.join( self.file, newname )
-        if not os.path.isfile(newname):
-            if os.path.isdir(newname):
-                created = True
-            else:
-                try:
-                   os.mkdir( newname )
-                   created = True
-                except os.error, detail:
-                   raise detail
-                   created = False
-    
-            if created:
-                nodeDir = Element("folder", name=newname)
-                self.__node.append( nodeDir )
-                return FolderNode(nodeDir)
+        ll=[i for i in self.getFolders() if i.file==newname]
+        if len(ll)==1:
+            return ll[0]
+        else:
+            if not os.path.isfile(newname):
+                if os.path.isdir(newname):
+                    created = True
+                else:
+                    try:
+                       os.mkdir( newname )
+                       created = True
+                    except os.error, detail:
+                       raise detail
+                       created = False
+        
+                if created:
+                    nodeDir = Element("folder", name=newname)
+                    self.__node.append( nodeDir )
+                    return FolderNode(nodeDir)
         return False
 
     def remove(self):
