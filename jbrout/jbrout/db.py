641b0190e90feb7e8941da7b6da6797b3c751336
ceplm@seznam.cz <ceplm@seznam.cz@b4d1b9c4-0e25-0410-a282-091a75307ab2>
2014-07-16 23:08:54 +0000

    Replug elimination of non-XML characters.
    git-svn-id: https://jbrout.googlecode.com/svn/trunk@375 b4d1b9c4-0e25-0410-a282-091a75307ab2

--- jbrout/jbrout/db.py
+++ jbrout/jbrout/db.py
@@ -32,6 +32,7 @@ import os.path
 from commongtk import Buffer, rgb, Img, MessageBoxScrolled
 
 from subprocess import Popen, PIPE
+import char_utils
 
 
 def walktree(top=".", depthfirst=True):
@@ -959,7 +960,13 @@ class PhotoNode(object):
                 self.__node.append(nodeTag)
         if pc.comment:
             nodeComment = Element("c")
-            nodeComment.text = pc.comment.replace(u'\x00', u' ')
+            try:
+                nodeComment.text = char_utils.make_xml_string_legal(pc.comment)
+            except ValueError, f:
+                print "exception = %s" % f
+                print "nodeComment = %s" % nodeComment
+                print "pc.comment = %s" % pc.comment
+                raise
             self.__node.append(nodeComment)
         if pc.rating:
             nodeRating = Element("r")
